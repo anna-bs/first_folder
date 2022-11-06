ab4bec748929e7d36fb0dc28b2aa1da00b011853
anna-bs <anna.disraeli@gmail.com>
2022-11-06 23:46:31 +0100

    Added unit conversion, weather icon, weather description
    - in HTML changed the units position
    - in JS added the SEARCH and HANDLESUBMIT functions

--- src/script.js
+++ src/script.js
@@ -15,7 +15,13 @@ let days = [
 ];
 let currentDay = days[now.getDay()];
 let currentHour = now.getHours();
-let currentMin = now.getMinutes();
+function addZero(sd) {
+  if (sd < 10) {
+    sd = "0" + sd;
+  }
+  return sd;
+}
+let currentMin = addZero(now.getMinutes());
 let currentDate = now.getDate();
 let months = [
   "Jan",
@@ -38,37 +44,62 @@ time.innerHTML = `${currentHour}:${currentMin}`;
 day.innerHTML = `${currentDay}`;
 date.innerHTML = `${currentDate} ${currentMonth} ${currentYear}`;
 
-function currentTemp(response) {
+function displayTemperature(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#selected-city");
   let currentTemp = document.querySelector("#temperature");
+  let feelsLike = document.querySelector("#feels-like");
+  let iconElement = document.querySelector("#icon");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+
+  celsiusTemperature = response.data.main.temp;
+
+  cityElement.innerHTML = response.data.name;
   currentTemp.innerHTML = Math.round(response.data.main.temp);
+  feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+  humidity.innerHTML = response.data.main.humidity;
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
-function searchCity(city) {
-  city.preventDefault();
+function handleSubmit(event) {
+  event.preventDefault();
   let searchInput = document.querySelector("#find-city");
-  let currentCity = document.querySelector("#selected-city");
+  search(searchInput.value);
+}
+
+function search(city) {
   let apiKey = "50fa4024e3b1d5eac2f51ab18a47e997";
-  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
-  axios.get(apiURL).then(currentTemp);
-  currentCity.innerHTML = `${searchInput.value}`;
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(displayTemperature);
 }
 
-let city = document.querySelector("#city-form");
-city.addEventListener("submit", searchCity);
-
-function celsius(tempC) {
-  tempC.preventDefault();
+function celsius(event) {
+  event.preventDefault();
+  celsiusUnit.classList.add("active");
+  fahrenheitUnit.classList.remove("active");
   let currentTemperature = document.querySelector("#temperature");
-  currentTemperature.innerHTML = 23;
+  currentTemperature.innerHTML = Math.round(celsiusTemperature);
 }
 
-function fahrenheit(tempF) {
-  tempF.preventDefault();
+function fahrenheit(event) {
+  event.preventDefault();
+  celsiusUnit.classList.remove("active");
+  fahrenheitUnit.classList.add("active");
   let currentTemperature = document.querySelector("#temperature");
-  let temperature = currentTemperature.innerHTML;
-  temperature = Number(temperature);
-  currentTemperature.innerHTML = Math.round((temperature * 9) / 5 + 32);
+  currentTemperature.innerHTML = Math.round((celsiusTemperature * 9) / 5 + 32);
 }
+
+let form = document.querySelector("#city-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Berlin");
+
+let celsiusTemperature = null;
 
 let celsiusUnit = document.querySelector("#celsius");
 celsiusUnit.addEventListener("click", celsius);
